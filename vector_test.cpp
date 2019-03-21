de6d9a4532a4caacbb53c914851bcc7e32b81657
stefanos-baziotis <sdi1600105@di.uoa.gr>
2019-03-21 21:18:27 +0200

    Format change

--- vector_test.cpp
+++ vector_test.cpp
@@ -5,30 +5,30 @@
 using namespace std;
 
 void helper(Vec<int> b) {
-	for(Vec<int>::iterator it = b.begin(); it != b.end(); ++it) {
-		*it += 1;
-	}
+    for (Vec<int>::iterator it = b.begin(); it != b.end(); ++it) {
+        *it += 1;
+    }
 
-	for(size_t i = 0; i != b.size(); ++i) {
-		cout << b[i] << endl;
-	}
-	cout << endl;
+    for (size_t i = 0; i != b.size(); ++i) {
+        cout << b[i] << endl;
+    }
+    cout << endl;
 }
 
 // TODO(stefanos): More tests.
 void test1(void) {
-	Vec<int> a;
+    Vec<int> a;
 
-	for(int i = 0; i != 10; ++i) {
-		a.push_back(i);
-	}
-	helper(a);
+    for (int i = 0; i != 10; ++i) {
+        a.push_back(i);
+    }
+    helper(a);
 
-	for(Vec<int>::const_iterator it = a.begin(); it != a.end(); ++it) {
-		cout << *it << endl;
-	}
+    for (Vec<int>::const_iterator it = a.begin(); it != a.end(); ++it) {
+        cout << *it << endl;
+    }
 }
 
 int main(void) {
-	test1();
+    test1();
 }
